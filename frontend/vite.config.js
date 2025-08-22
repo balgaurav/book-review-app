1846ee0e77d79e2b6e25d2471391873b7f13fb06
Gaurav Bal <gbal@uwaterloo.ca>
2025-08-22 00:30:36 -0400

    feat: Set up React frontend with Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- ADD THIS LINE
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- ADD THIS LINE
-  ],
+  plugins: [react()],
 })
